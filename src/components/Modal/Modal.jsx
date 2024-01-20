1c21c45cdf1a743ebf70dd3c1dec393eacabcf2e
Roki2221 <100676027+Roki2221@users.noreply.github.com>
2024-01-20 14:46:28 +0200

    modal refactored

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,27 +1,28 @@
 import css from './style.module.css';
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 
-export default class Modal extends Component {
-  handleClick = e => {
-    e.target === e.currentTarget && this.props.close();
+function Modal(props) {
+  useEffect(() => {
+    document.addEventListener('keydown', handleEsc);
+    return () => {
+      document.removeEventListener('keydown', handleEsc);
+    };
+  }, []);
+
+  const handleClick = e => {
+    e.target === e.currentTarget && props.close();
   };
-  handleEsc = e => {
-    e.code === 'Escape' && this.props.close();
+  const handleEsc = e => {
+    e.code === 'Escape' && props.close();
     console.log('first');
   };
-  componentDidMount() {
-    document.addEventListener('keydown', this.handleEsc);
-  }
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleEsc);
-  }
-  render() {
-    return (
-      <div className={css.Overlay} onClick={this.handleClick}>
-        <div className={css.Modal}>
-          <img src={this.props.openedImage} alt="" />
-        </div>
+  return (
+    <div className={css.Overlay} onClick={handleClick}>
+      <div className={css.Modal}>
+        <img src={props.openedImage} alt="" />
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default Modal;
